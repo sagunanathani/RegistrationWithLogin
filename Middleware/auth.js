9339b099436691168cbb19d8426ba18a3e53e748
the_saguna_nathani <104073036+sagunanathani@users.noreply.github.com>
2022-08-18 10:27:39 +0530

    Changes in Auth file in middleware

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -3,43 +3,34 @@ const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 
 //post login validation
-module.exports = async (req, res,next) => {
+module.exports = async (req, res, next) => {
     console.log("login details------------>", req.body);
     try {
-        // const token = req.headers.authorization.split(' ')[1];
-        // const decodedToken = jwt.verify(token, 'sagu');
-
         const bearerHeader = req.body.token || req.query.token || req.headers['authorization'];
-
-        console.log("bearerHeader in validetior----->",bearerHeader)
-
-        if(typeof bearerHeader !== 'undefined')
-        {
-
+        console.log("bearerHeader in validator----->", bearerHeader);
+        if (typeof bearerHeader !== 'undefined') {
             // const bearer = bearerHeader.split(' ');
             // const token=bearer[1];
             const token = bearerHeader;
-            console.log("token in validatior--->",token)
+            console.log("token in validator--->", token);
 
-            jwt.verify(token, "sagu" ,(err,authData)=>{
-                if(err)
-                {
+            // sagu is a key and also I have pass in login routes for jwt token
+            jwt.verify(token, "sagu", (err, authData) => {
+                if (err) {
                     // res.json({result:err})
-                    console.log("error of jwt verify--->",err);
-                    return res.json( {
+                    console.log("error of jwt verify--->", err);
+                    return res.json({
                         success: false,
                         message: "Failed to authenticate token.",
-                    } );
-                }else{
+                    });
+                } else {
                     next();
                 }
             })
+        } else {
+            res.send({"result": "Token not provided"});
         }
-        else{
-            res.send({"result":"Token not provied"});
-        }
-
-    }catch (e) {
-        res.status(400).send({error:e});
+    } catch (e) {
+        res.status(400).send({error: e});
     }
 };
